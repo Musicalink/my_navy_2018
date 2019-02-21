bbbda2158950703f40bcab518f1d182377a86b50
paul.bryche@epitech.eu <paul.bryche@epitech.eu>
2019-02-21 19:46:06 +0100

    [+]check_map.c checking ships pos done

---
+++ src/check_map.c
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2019
+** check_map
+** File description:
+** paul bryche
+*/
+
+#include <navy.h>
+
+int check_map_positions(int value)
+{
+    int x = 2;
+    int y = 2;
+    char c = value + 30;
+
+    while (table[y] != NULL) {
+        while (tabl[y][x] != '\0') {
+            if (tabl[y][x] == c)
+                value --;
+            x++;
+        }
+        y++;
+        x = 2;
+    }
+    if (value != 0)
+        return (84);
+    return (0);
+}
+
+int check_map(void)
+{
+    int counter = 2;
+
+    while (counter <= 5) {
+        if (check_map_positions(counter) == 0)
+            counter ++;
+        else
+            return (84);
+    }
+    return (0);
+}
